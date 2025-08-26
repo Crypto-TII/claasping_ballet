d20e1627656af220355f2cd05bc097aeae3ca334
gabriele-bellini <216871851+gabriele-bellini@users.noreply.github.com>
2025-08-26 12:44:37 +0200

    refactor: minor changes to simplify the execution in fixed differential cryptanalysis, Ballet

--- ballet_unimi/scripts/find_lowest_weight_xor_differentialfix128128_trail_ballet.py
+++ ballet_unimi/scripts/find_lowest_weight_xor_differentialfix128128_trail_ballet.py
@@ -6,7 +6,7 @@ from claasp.cipher_modules.models.sat.sat_models.sat_xor_differential_model impo
 from claasp.ciphers.block_ciphers.ballet_block_cipher import BalletBlockCipher
 from claasp.cipher_modules.models.utils import set_fixed_variables, integer_to_bit_list
 
-solver = "PARKISSAT_EXT" # use 100 core
+solver = "PARKISSAT_EXT" # use 50 core
 
 # 7 round trail used as a possible starting point for the search
 intermediate_outputs_7round = {
@@ -63,7 +63,6 @@ intermediate_outputs_9round = {
     'intermediate_output_8_9':  '0x00000000000000000000000000000000'
 }
 block_bit, key_bit = (128,128)
-intermediate_outputs = intermediate_outputs_7round
 
 
 
@@ -72,37 +71,38 @@ def hex_to_bitlist(hex_str):
     bit_len = (len(hex_str) - 2) * 4
     return integer_to_bit_list(val_int, bit_len, "big")
 
-for round in range(8, 16): 
-    filename= f"find_lowest_weight_xor_differential_trail_starting_from_given_trail__ballet_{block_bit}block_{key_bit}key_{round}rounds7fixed__{solver}solver__50thread.json"
-    start_date = datetime.datetime.now()
+for round in range(10, 15): 
+    for intermediate_outputs, num_fix_round in [(intermediate_outputs_9round,9), (intermediate_outputs_8round,8), (intermediate_outputs_7round,7)]:
+        filename= f"find_lowest_weight_xor_differential_trail_starting_from_given_trail__ballet_{block_bit}block_{key_bit}key_{round}rounds{num_fix_round}fixed__{solver}solver__50thread.json"
+        start_date = datetime.datetime.now()
 
-    ballet = BalletBlockCipher(block_bit_size=block_bit, key_bit_size=key_bit, number_of_rounds=round)
-    sat = SatXorDifferentialModel(ballet)
+        ballet = BalletBlockCipher(block_bit_size=block_bit, key_bit_size=key_bit, number_of_rounds=round)
+        sat = SatXorDifferentialModel(ballet)
 
-    fixed_values = []
-    fixed_values.append(set_fixed_variables('key', 'equal', list(range(key_bit)), integer_to_bit_list(0, key_bit, 'big')))
-    fixed_values.append(set_fixed_variables('plaintext', 'not_equal', list(range(block_bit)), integer_to_bit_list(0, block_bit, 'big')))
+        fixed_values = []
+        fixed_values.append(set_fixed_variables('key', 'equal', list(range(key_bit)), integer_to_bit_list(0, key_bit, 'big')))
+        fixed_values.append(set_fixed_variables('plaintext', 'not_equal', list(range(block_bit)), integer_to_bit_list(0, block_bit, 'big')))
 
-    for var_name, hex_val in intermediate_outputs.items():
-        bits = hex_to_bitlist(hex_val)
-        bit_indices = list(range(len(bits)))
-        fixed_values.append(set_fixed_variables(var_name, 'equal', bit_indices, bits))
+        for var_name, hex_val in intermediate_outputs.items():
+            bits = hex_to_bitlist(hex_val)
+            bit_indices = list(range(len(bits)))
+            fixed_values.append(set_fixed_variables(var_name, 'equal', bit_indices, bits))
 
-    try:
-        trail = sat.find_lowest_weight_xor_differential_trail(fixed_values, solver_name=solver)
-        trail["cipher"] = str(trail["cipher"])
-    except Exception as e:
-        errorMessage = f"args={sys.argv}\nException occurred during SAT: {repr(e)}\n\n" + f"Traceback:\n{traceback.format_exc()}" + "-"*60 + f" start={start_date} end={datetime.datetime.now()}\n\n"
-        with open(filename,"a") as f:
-            f.write(errorMessage)
-        with open("errors.log","a") as f:
-            f.write(errorMessage)
-        break
+        try:
+            trail = sat.find_lowest_weight_xor_differential_trail(fixed_values, solver_name=solver)
+            trail["cipher"] = str(trail["cipher"])
+        except Exception as e:
+            errorMessage = f"args={sys.argv}\nException occurred during SAT: {repr(e)}\n\n" + f"Traceback:\n{traceback.format_exc()}" + "-"*60 + f" start={start_date} end={datetime.datetime.now()}\n\n"
+            with open(filename,"a") as f:
+                f.write(errorMessage)
+            with open("errors.log","a") as f:
+                f.write(errorMessage)
+            break
 
-    try: 
-        with open(filename,"a") as f:
-            f.write(json.dumps(trail, indent=4))
-    except Exception as e:
-        errorMessage = f"args={sys.argv}\nException occurred during result saving: {repr(e)}\n\n" + f"Traceback:\n{traceback.format_exc()}" + "-"*60 + f" start={start_date} end={datetime.datetime.now()}\n\n"
-        with open("errors.log","a") as f:
-            f.write(errorMessage)
+        try: 
+            with open(filename,"w") as f:
+                f.write(json.dumps(trail, indent=4))
+        except Exception as e:
+            errorMessage = f"args={sys.argv}\nException occurred during result saving: {repr(e)}\n\n" + f"Traceback:\n{traceback.format_exc()}" + "-"*60 + f" start={start_date} end={datetime.datetime.now()}\n\n"
+            with open("errors.log","a") as f:
+                f.write(errorMessage)
--- ballet_unimi/scripts/find_lowest_weight_xor_differentialfix256256_trail_ballet.py
+++ ballet_unimi/scripts/find_lowest_weight_xor_differentialfix256256_trail_ballet.py
@@ -8,24 +8,25 @@ from claasp.cipher_modules.models.utils import set_fixed_variables, integer_to_b
 
 solver = "PARKISSAT_EXT"
 
-intermediate_outputs = {
-    "intermediate_output_0_9":  "0x00000000000000000000000000000000",
+intermediate_outputs_8round = {
+    "intermediate_output_0_9":  "0x0000000000000000000000000000000000000000000000000000000000000000",
     "intermediate_output_0_10": "0x0202201000000000004400000000000000044002000000000020200000000000",
-    "intermediate_output_1_9":  "0x00000000000000000000000000000000",
+    "intermediate_output_1_9":  "0x0000000000000000000000000000000000000000000000000000000000000000",
     "intermediate_output_1_10": "0x0044000000000000000800000000000000008000000000000004400200000000",
-    "intermediate_output_2_9": "0x00000000000000000000000000000000",
+    "intermediate_output_2_9":  "0x0000000000000000000000000000000000000000000000000000000000000000",
     "intermediate_output_2_10": "0x0008000000000000000000000000000000010000000000000000800000000000",
-    "intermediate_output_3_9": "0x00000000000000000000000000000000",
+    "intermediate_output_3_9":  "0x0000000000000000000000000000000000000000000000000000000000000000",
     "intermediate_output_3_10": "0x0000000000000000000000000000000000000000000000000001000000000000",
-    "intermediate_output_4_9": "0x00000000000000000000000000000000",
+    "intermediate_output_4_9":  "0x0000000000000000000000000000000000000000000000000000000000000000",
     "intermediate_output_4_10": "0x0000000000000000000000000000000080000000000000000000000000000000",
-    "intermediate_output_5_9": "0x00000000000000000000000000000000",
+    "intermediate_output_5_9":  "0x0000000000000000000000000000000000000000000000000000000000000000",
     "intermediate_output_5_10": "0x0000000000000000000000000000010000000000000020008000000000000000",
-    "intermediate_output_6_9": "0x00000000000000000000000000000000",
+    "intermediate_output_6_9":  "0x0000000000000000000000000000000000000000000000000000000000000000",
     "intermediate_output_6_10": "0x0000000000000100000000000042000000000000084040000000000000002000",
-    "intermediate_output_7_9": "0x00000000000000000000000000000000"
+    "intermediate_output_7_9":  "0x0000000000000000000000000000000000000000000000000000000000000000"
 }
-
+block_bit, key_bit = (256,256)
+intermediate_outputs = intermediate_outputs_8round
 
 
 def hex_to_bitlist(hex_str):
@@ -34,37 +35,36 @@ def hex_to_bitlist(hex_str):
     return integer_to_bit_list(val_int, bit_len, "big")
 
 for round in range(9, 14): 
-    for block_bit, key_bit in [(256,256)]:
-        start_date = datetime.datetime.now()
+    filename = f"find_lowest_weight_xor_differential_trail_starting_from_given_trail__ballet_{block_bit}block_{key_bit}key_{round}rounds7fixed__{solver}solver__50thread.json"
+    start_date = datetime.datetime.now()
 
-        ballet = BalletBlockCipher(block_bit_size=block_bit, key_bit_size=key_bit, number_of_rounds=round)
-        sat = SatXorDifferentialModel(ballet)
+    ballet = BalletBlockCipher(block_bit_size=block_bit, key_bit_size=key_bit, number_of_rounds=round)
+    sat = SatXorDifferentialModel(ballet)
 
-        fixed_values = []
-        fixed_values.append(set_fixed_variables('key', 'equal', list(range(key_bit)), integer_to_bit_list(0, key_bit, 'big')))
-        fixed_values.append(set_fixed_variables('plaintext', 'not_equal', list(range(block_bit)), integer_to_bit_list(0, block_bit, 'big')))
+    fixed_values = []
+    fixed_values.append(set_fixed_variables('key', 'equal', list(range(key_bit)), integer_to_bit_list(0, key_bit, 'big')))
+    fixed_values.append(set_fixed_variables('plaintext', 'not_equal', list(range(block_bit)), integer_to_bit_list(0, block_bit, 'big')))
 
-        for var_name, hex_val in intermediate_outputs.items():
-            bits = hex_to_bitlist(hex_val)
-            bit_indices = list(range(len(bits)))
-            fixed_values.append(set_fixed_variables(var_name, 'equal', bit_indices, bits))
+    for var_name, hex_val in intermediate_outputs.items():
+        bits = hex_to_bitlist(hex_val)
+        bit_indices = list(range(len(bits)))
+        fixed_values.append(set_fixed_variables(var_name, 'equal', bit_indices, bits))
 
-        try:
-            trail = sat.find_lowest_weight_xor_differential_trail(fixed_values, solver_name=solver)
-            trail["cipher"] = str(trail["cipher"])
-        except Exception as e:
-            errorMessage = f"args={sys.argv}\nException occurred during SAT: {repr(e)}\n\n" + f"Traceback:\n{traceback.format_exc()}" + "-"*60 + f" start={start_date} end={datetime.datetime.now()}\n\n"
-            with open(f"find_lowest_weight_xor_differential_trail__ballet_{block_bit}block_{key_bit}key_{round}rounds__{solver}solver.txt","a") as f:
-                f.write(errorMessage)
-            with open("errors.log","a") as f:
-                f.write(errorMessage)
-            break
+    try:
+        trail = sat.find_lowest_weight_xor_differential_trail(fixed_values, solver_name=solver)
+        trail["cipher"] = str(trail["cipher"])
+    except Exception as e:
+        errorMessage = f"args={sys.argv}\nException occurred during SAT: {repr(e)}\n\n" + f"Traceback:\n{traceback.format_exc()}" + "-"*60 + f" start={start_date} end={datetime.datetime.now()}\n\n"
+        with open(filename,"a") as f:
+            f.write(errorMessage)
+        with open("errors.log","a") as f:
+            f.write(errorMessage)
+        break
 
-        try: 
-            with open(f"find_lowest_weight_xor_differential_trail__ballet_{block_bit}block_{key_bit}key_{round}rounds__{solver}solver.txt","a") as f:
-                f.write(json.dumps(trail, indent=4))
-                f.write("-"*60 + f" start={start_date} end={datetime.datetime.now()}\n\n")
-        except Exception as e:
-            errorMessage = f"args={sys.argv}\nException occurred during result saving: {repr(e)}\n\n" + f"Traceback:\n{traceback.format_exc()}" + "-"*60 + f" start={start_date} end={datetime.datetime.now()}\n\n"
-            with open("errors.log","a") as f:
-                f.write(errorMessage)
+    try: 
+        with open(filename,"w") as f:
+            f.write(json.dumps(trail, indent=4))
+    except Exception as e:
+        errorMessage = f"args={sys.argv}\nException occurred during result saving: {repr(e)}\n\n" + f"Traceback:\n{traceback.format_exc()}" + "-"*60 + f" start={start_date} end={datetime.datetime.now()}\n\n"
+        with open("errors.log","a") as f:
+            f.write(errorMessage)
