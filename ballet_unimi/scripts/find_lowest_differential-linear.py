0051fa2a8a54fcb922b25c5258e38dc4c676f151
Michela Gallone <michela.gallone@studenti.unimi.it>
2025-11-20 16:08:17 +0100

    Generalize the differential linear file

--- ballet_unimi/scripts/find_lowest_differential-linear.py
+++ ballet_unimi/scripts/find_lowest_differential-linear.py
@@ -1,31 +1,35 @@
-import sys
 import json
-import traceback
-import datetime
 import itertools
-import math
 
-from claasp.cipher_modules.models.sat.sat_models.sat_differential_linear_model import SatDifferentialLinearModel
+
+from claasp.cipher_modules.models.sat.sat_models.sat_differential_linear_model import (
+    SatDifferentialLinearModel,
+)
 
 from claasp.ciphers.block_ciphers.ballet_block_cipher import BalletBlockCipher
 from claasp.cipher_modules.models.utils import set_fixed_variables, integer_to_bit_list
-from claasp.cipher_modules.models.utils import differential_linear_checker_for_block_cipher_single_key
+from claasp.cipher_modules.models.utils import (
+    differential_linear_checker_for_block_cipher_single_key,
+)
+
 solver = "PARKISSAT_EXT"
-number_of_rounds = 9
+round_differential = 4
+round_intermediate = 1
+round_linear = 4
+number_of_rounds = round_differential + round_intermediate + round_linear
 
 for block_bit, key_bit in [(128, 128), (128, 256), (256, 256)]:
-    start_date = datetime.datetime.now()
     ballet = BalletBlockCipher(
-        block_bit_size=block_bit, key_bit_size=key_bit, number_of_rounds=9
+        block_bit_size=block_bit, key_bit_size=key_bit, number_of_rounds=number_of_rounds
     )
 
     middle_part_components = []
     bottom_part_components = []
 
-    for round_number in range(4, 5):
+    for round_number in range(round_differential, round_differential + round_intermediate):
         middle_part_components.append(ballet.get_components_in_round(round_number))
 
-    for round_number in range(5, 9):
+    for round_number in range(round_differential + round_intermediate, number_of_rounds):
         bottom_part_components.append(ballet.get_components_in_round(round_number))
 
     middle_part_components = list(itertools.chain(*middle_part_components))
@@ -65,36 +69,36 @@ for block_bit, key_bit in [(128, 128), (128, 256), (256, 256)]:
         component_model_list,
     )
 
-
     trail = sat.find_lowest_weight_xor_differential_linear_trail(
         fixed_values=[key, plaintext, ciphertext_difference],
         solver_name=solver,
-        num_unknown_vars=block_bit-1,
+        num_unknown_vars=block_bit - 1,
     )
     trail["cipher"] = str(trail["cipher"])
-   
-    input_difference_str = trail['components_values']['plaintext']['value']
-    output_mask_str = trail['components_values']['cipher_output_8_10']['value']
-    key_str = trail['components_values']['key']['value']
 
-    input_difference = int(input_difference_str, 16)
-    output_mask =int(output_mask_str, 16)
-    key=int(key_str, 16)
-    number_of_samples = 2 ** 18
-    number_of_rounds = 9
-   
-    corr = differential_linear_checker_for_block_cipher_single_key(
-        ballet, input_difference, output_mask, number_of_samples, block_bit, key_bit, key
-    )
+    # parte di verifica del trail
 
-    abs_corr = abs(corr)
-    print("Correlation:", abs(math.log(abs_corr, 2)))
+    if number_of_rounds <= 9:
+        input_difference_str = trail["components_values"]["plaintext"]["value"]
+        output_mask_str = trail["components_values"][f"cipher_output_{number_of_rounds - 1}_10"][
+            "value"
+        ]
+        key_str = trail["components_values"]["key"]["value"]
 
+        input_difference = int(input_difference_str, 16)
+        output_mask = int(output_mask_str, 16)
+        key = int(key_str, 16)
+        number_of_samples = 2 ** (2 * int(trail["total_weight"]))
+
+        corr = differential_linear_checker_for_block_cipher_single_key(
+            ballet, input_difference, output_mask, number_of_samples, block_bit, key_bit, key
+        )
+
+        abs_corr = abs(corr)
+        assert abs_corr < trail["total_weight"], "trail not verfied"
 
     with open(
-        f"find_lowest_weight_xor_differentiallinear_trail__ballet_{block_bit}block_{key_bit}key_9 rounds 4-1-4__{solver}solver.txt",
-        "a",
+        f"find_lowest_weight_xor_differentiallinear_trail__ballet_{block_bit}block_{key_bit}key_{number_of_rounds}_rounds{round_differential}-{round_intermediate}-{round_linear}__{solver}solver.txt",
+        "w",
     ) as f:
         f.write(json.dumps(trail, indent=4))
-
-
